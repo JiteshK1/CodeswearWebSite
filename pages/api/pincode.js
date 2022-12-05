33d5317ef2abc8faf5c26f0de049de649cdf3d06
JiteshK1 <kanyaljitesh0@gmail.com>
2022-12-06 01:11:22 +0530

    displaying Tshirts variant-1

--- pages/api/pincode.js
+++ pages/api/pincode.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json([12212, 44421, 232321]);
+  res.status(200).json([12212, 44421, 232321, 416520, 123456]);
 }
